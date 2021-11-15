602e6db4426f88094b2b7f024cf77e03e7be880a
margaza18 <90214085+margaza18@users.noreply.github.com>
2021-11-15 16:22:41 +0200

    part1

---
+++ laba5_5.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+
+filename="first_group.txt"
+text_file=open(filename)
+a=int(input())
+line = open("C:\\Users\\Пользователь\\Desktop\\laba5\\first_group.txt")
+print(line)
+text_file.close()
+
+filename="second_group.txt"
+text_file=open(filename)
+a=int(input())
+line = open("C:\\Users\\Пользователь\\Desktop\\laba5\\second_group.txt")
+print(line)
+text_file.close()
